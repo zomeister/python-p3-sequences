e06d14a720302cf2d6b095945378b82b958880a0
Zoe Guidry <oyezoey@gmail.com>
2023-05-26 19:35:26 -0500

    lab completed. maybe should review.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci_sequence = []
+    if length > 0:
+        fibonacci_sequence.append(0)
+        if length > 1:
+            fibonacci_sequence.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonacci_sequence.append(
+                        fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+
+    print(fibonacci_sequence)
     pass
